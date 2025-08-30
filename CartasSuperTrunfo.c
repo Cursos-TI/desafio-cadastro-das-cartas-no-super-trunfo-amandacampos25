bb06c37c0d2a3f7610c752cacee731f1d9b25243
Amanda Ellen de Campos <amandaellendecampos@gmail.com>
2025-08-30 00:36:36 +0000

    entrada de dados referente as informações das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -26,6 +26,55 @@ int main(){
     float pib2;
     int pontos_turisticos2;
 
+//Iniciando o jogo colocando as informações da primeira carta
+
+    printf("Primeira carta \n");
+
+    printf("Digite uma letra de 'A' a 'H':");
+    scanf("%c", &estado1);
+
+    printf("Digite o código da carta (1 até 4):");
+    scanf("%s", &codigo1);
+
+    printf("Insira o nome da cidade:");
+    scanf("%s", &cidade1);
+
+    printf("Digite qual é a população da cidade:");
+    scanf("%d", &populacao1);
+
+    printf("Digite qual é a área da cidade (em km²):");
+    scanf("%f", &area1);
+
+    printf("Digite o PIB:");
+    scanf("%f", &pib1);
+
+    printf("Insira quantos pontos turisticos essa cidade possui:");
+    scanf("%d", &pontos_turisticos1);
+
+//inserindo informações sobre a segunda carta
+
+    printf("Segunda carta\n");
+
+    printf("Digite uma letra de 'A' a 'H':");
+    scanf("%c", &estado2);
+
+    printf("Digite o código da carta (1 até 4):");
+    scanf("%s", &codigo2);
+
+    printf("Insira o nome da cidade:");
+    scanf("%s", &cidade2);
+
+    printf("Digite qual é a população da cidade:");
+    scanf("%d", &populacao2);
+
+    printf("Digite qual é a área da cidade (em km²):");
+    scanf("%f", &area2);
+
+    printf("Digite o PIB:");
+    scanf("%f", &pib2);
+
+    printf("Insira quantos pontos turisticos essa cidade possui:");
+    scanf("%d", &pontos_turisticos2);
 
 
 
