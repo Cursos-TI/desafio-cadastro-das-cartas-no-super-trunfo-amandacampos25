b6c557d9e6d48b594fd15c8338b8d9bee2820ecd
Amanda Ellen de Campos <amandaellendecampos@gmail.com>
2025-08-30 01:23:13 +0000

    Entrega do desafio nivel: novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,85 +1,98 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-
-
-int main(){
-
-//Informações carta 1
+int main() {
+    //carta 1
     char estado1;
     char codigo1[5];
-    char cidade1;
+    char cidade1[20];
     int populacao1;
     float area1;
     float pib1;
     int pontos_turisticos1;
 
-//Informações da carta 2
+    //carta2
     char estado2;
     char codigo2[5];
-    char cidade2;
+    char cidade2[20];
     int populacao2;
     float area2;
     float pib2;
     int pontos_turisticos2;
 
-//Iniciando o jogo colocando as informações da primeira carta
+    //Iniciando o jogo colocando as informações da primeira carta
 
     printf("Primeira carta \n");
 
-    printf("Digite uma letra de 'A' a 'H':");
-    scanf("%c", &estado1);
+    printf("Digite uma letra de 'A' a 'H':\n");
+    scanf(" %c", &estado1);
 
-    printf("Digite o código da carta (1 até 4):");
-    scanf("%s", &codigo1);
+    printf("Digite o código da carta (A letra do estado e um número de 1 a 4):\n");
+    scanf("%s", codigo1);
 
-    printf("Insira o nome da cidade:");
-    scanf("%s", &cidade1);
+    printf("Insira o nome da cidade:\n");
+    scanf("%s", cidade1);
 
-    printf("Digite qual é a população da cidade:");
+    printf("Digite qual é a população da cidade:\n");
     scanf("%d", &populacao1);
 
-    printf("Digite qual é a área da cidade (em km²):");
+    printf("Digite qual é a área da cidade (em km²):\n");
     scanf("%f", &area1);
 
-    printf("Digite o PIB:");
+    printf("Digite o PIB:\n");
     scanf("%f", &pib1);
 
-    printf("Insira quantos pontos turisticos essa cidade possui:");
+    printf("Insira quantos pontos turisticos essa cidade possui:\n");
     scanf("%d", &pontos_turisticos1);
 
-//inserindo informações sobre a segunda carta
 
-    printf("Segunda carta\n");
+    //inserindo informações sobre a segunda carta
 
-    printf("Digite uma letra de 'A' a 'H':");
-    scanf("%c", &estado2);
+    printf("Segunda carta \n");
 
-    printf("Digite o código da carta (1 até 4):");
-    scanf("%s", &codigo2);
+    printf("Digite uma letra de 'A' a 'H':\n");
+    scanf(" %c", &estado2);
 
-    printf("Insira o nome da cidade:");
-    scanf("%s", &cidade2);
+    printf("Digite o código da carta (A letra do estado e um número de 1 a 4):\n");
+    scanf("%s", codigo2);
 
-    printf("Digite qual é a população da cidade:");
+    printf("Insira o nome da cidade:\n");
+    scanf("%s", cidade2);
+
+    printf("Digite qual é a população da cidade:\n");
     scanf("%d", &populacao2);
 
-    printf("Digite qual é a área da cidade (em km²):");
+    printf("Digite qual é a área da cidade (em km²):\n");
     scanf("%f", &area2);
 
-    printf("Digite o PIB:");
+    printf("Digite o PIB:\n");
     scanf("%f", &pib2);
 
-    printf("Insira quantos pontos turisticos essa cidade possui:");
+    printf("Insira quantos pontos turisticos essa cidade possui:\n");
     scanf("%d", &pontos_turisticos2);
 
 
+    //Resultado do jogo - mostrando informações das cartas
 
+    printf("CARTA 1\n");
+    printf("Estado: %c \n", estado1);
+    printf("Código: %s \n", codigo1);
+    printf("Nome da Cidade: %s \n", cidade1);
+    printf("População: %d \n", populacao1);
+    printf("Área: %f \n", area1);
+    printf("PIB: %f \n", pib1);
+    printf("Pontos Turisticos: %d \n", pontos_turisticos1);
 
+    printf("\n");
 
+    printf("CARTA 2\n");
+    printf("Estado: %c \n", estado2);
+    printf("Código: %s \n", codigo2);
+    printf("Nome da Cidade: %s \n", cidade2);
+    printf("População: %d \n", populacao2);
+    printf("Área: %f \n", area2);
+    printf("PIB: %f \n", pib2);
+    printf("Pontos Turisticos: %d \n", pontos_turisticos2);
 
+    //Desafio novato
     return 0;
 }
